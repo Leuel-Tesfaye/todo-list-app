ed983c946236e192e2f3c597edd5c477b161c471
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-18 10:08:17 +0300

    to do items change commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
   return (
     <div className="App">
-     <h1>My Todo's</h1>
-     <div className='todo-wrapper'>
-     <div className='todo-input'>   
-      <div class="todo-input-item">
-        <label >Title</label>
-        <input type="text" placeholder="What's the task title"/>
-      </div>
+      <h1>My Todo's</h1>
+      <div className='todo-wrapper'>
+        <div className='todo-input'>   
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title"/>
+          </div>
 
-      <div class="todo-input-item">
-        <label >Description</label>
-        <input type="text" placeholder="What's the task Description"/>
-      </div>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="What's the task Description"/>
+          </div>
 
-      <div class="todo-input-item">
-        <label >Description</label>
-        <button type="button" className='primaryBtn'> Add</button>
-      </div>
-    </div>
-    <div className='btn-area'>
-        <button>Todo</button>
-        <button>completed</button>
-    </div>
-
-    <div className='todo-list'>
-        <div className='todo-list-item'>
-          <h3>Task 1</h3>
-          <p>Description</p>
+          <div className="todo-input-item">
+            <button type="button" className="button">
+              <span className="button__text">Add </span>
+              <span className="button__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round" stroke="currentColor" height="24" fill="none" className="svg">
+                  <line y2="19" y1="5" x2="12" x1="12"></line>
+                  <line y2="12" y1="12" x2="19" x1="5"></line>
+                </svg>
+              </span>
+            </button>
+          </div>
         </div>
-    </div>
-     </div>
+        <div className='btn-area'>
+          <button className={`secondaryBtn ${isCompleteScreen === false && 'active'}`} 
+            onClick={() => setIsCompleteScreen(false)}>
+            Todo
+          </button>
+
+          <button className={`secondaryBtn ${isCompleteScreen === true && 'active'}`} 
+            onClick={() => setIsCompleteScreen(true)}>
+            Completed
+          </button>
+        </div>
+
+        <div className='todo-list'>
+          <div className='todo-list-item'>
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
