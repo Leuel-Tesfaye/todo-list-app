45133a9925d2e3ab904531718f0c18f8f8040189
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-18 16:34:45 +0300

    icons css and html change commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,71 @@
-import { useState,useEffect } from 'react';
-import './App.css';
-import { MdDelete } from 'react-icons/md';
-import { BsCheckLg } from 'react-icons/bs';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { MdDelete } from "react-icons/md";
+import { BsCheckLg } from "react-icons/bs";
 function App() {
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
-  const [allTodo, setTodo] = useState([]) ;
+  const [allTodo, setTodo] = useState([]);
   const [newTitle, setNewTitle] = useState("");
   const [newDescription, setNewDescription] = useState("");
-  const handleAddTodo =() =>{
+  const handleAddTodo = () => {
     let newTodoItem = {
-      title:newTitle, 
-      description : newDescription
-    }
+      title: newTitle,
+      description: newDescription,
+    };
     let updatedTodoArr = [...allTodo];
     updatedTodoArr.push(newTodoItem);
     setTodo(updatedTodoArr);
-    localStorage.setItem('todoList', JSON.stringify(updatedTodoArr))
-  }
+    localStorage.setItem("todoList", JSON.stringify(updatedTodoArr));
+  };
 
   /** use effect : when ever the page is loaded for the first time */
-    useEffect (() =>{
-      let savedTodo = JSON.parse(localStorage.getItem('todoList'))
-      if(savedTodo) {
-        setTodo(savedTodo)
-      }
-    },[])
+  useEffect(() => {
+    let savedTodo = JSON.parse(localStorage.getItem("todoList"));
+    if (savedTodo) {
+      setTodo(savedTodo);
+    }
+  }, []);
   return (
     <div className="App">
       <h1>My Todo's</h1>
-      <div className='todo-wrapper'>
-        <div className='todo-input'>   
+      <div className="todo-wrapper">
+        <div className="todo-input">
           <div className="todo-input-item">
             <label>Title</label>
-            <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="Enter a task ..."/>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder="Enter a task ..."
+            />
           </div>
 
           <div className="todo-input-item">
             <label>Description</label>
-            <input type="text" value={newDescription} onChange={(e) =>setNewDescription (e.target.value)} placeholder=" Task Description...."/>
+            <input
+              type="text"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+              placeholder=" Task Description...."
+            />
           </div>
 
           <div className="todo-input-item">
             <button type="button" className="button" onClick={handleAddTodo}>
               <span className="button__text">Add </span>
               <span className="button__icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round" stroke="currentColor" height="24" fill="none" className="svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  stroke="currentColor"
+                  height="24"
+                  fill="none"
+                  className="svg"
+                >
                   <line y2="19" y1="5" x2="12" x1="12"></line>
                   <line y2="12" y1="12" x2="19" x1="5"></line>
                 </svg>
@@ -52,34 +73,44 @@ function App() {
             </button>
           </div>
         </div>
-        <div className='btn-area'>
-          <button className={`secondaryBtn ${isCompleteScreen === false && 'active'}`} 
-            onClick={() => setIsCompleteScreen(false)}>
+        <div className="btn-area">
+          <button
+            className={`secondaryBtn ${isCompleteScreen === false && "active"}`}
+            onClick={() => setIsCompleteScreen(false)}
+          >
             Todo
           </button>
 
-          <button className={`secondaryBtn ${isCompleteScreen === true && 'active'}`} 
-            onClick={() => setIsCompleteScreen(true)}>
+          <button
+            className={`secondaryBtn ${isCompleteScreen === true && "active"}`}
+            onClick={() => setIsCompleteScreen(true)}
+          >
             Completed
           </button>
         </div>
 
-        <div className='todo-list'>
-          {allTodo.map((item,index) =>{
+        <div className="todo-list">
+          {allTodo.map((item, index) => {
             return (
-              <div className='todo-list-item'>
-            <div>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-            </div>
-            <div>
-          <MdDelete className= 'icon' />
-          <BsCheckLg className= 'check-icon' />
-          </div>
-          </div>
-            )
+              <div className="todo-list-item">
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <div className="icons">
+                  <button class="delete-button">
+                    <svg class="delete-svgIcon" viewBox="0 0 448 512">
+                      <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+                    </svg>
+                  </button>
+                  <label class="container">
+                    <input type="checkbox" checked="checked" />
+                    <div class="checkMark"></div>
+                  </label>
+                </div>
+              </div>
+            );
           })}
-          
         </div>
       </div>
     </div>
